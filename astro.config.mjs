dbce90d34d918b8f5921b0e852023b3d8aa5197f
Roy Duran <gcduranroldan@gmail.com>
2026-03-11 16:00:30 +0100

    configuraciones deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site:"https://github.com/RoyDuran",
+  base:"/CVRoy/",
   vite: {
     plugins: [tailwindcss()]
   }
